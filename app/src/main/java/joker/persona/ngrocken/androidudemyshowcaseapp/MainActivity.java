15bd1489b6aedcd17e98d15ce856940ee677600c
Ken Ng <ng.ken84@gmail.com>
2018-09-26 21:36:00 -0700

    first commit

---
+++ app/src/main/java/joker/persona/ngrocken/androidudemyshowcaseapp/MainActivity.java
@@ -0,0 +1,13 @@
+package joker.persona.ngrocken.androidudemyshowcaseapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
