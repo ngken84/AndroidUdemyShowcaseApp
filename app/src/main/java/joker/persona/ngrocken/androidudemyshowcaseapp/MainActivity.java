994f552c2053776a8226a74da51fde15ca2da5ce
Ken Ng <ng.ken84@gmail.com>
2018-09-27 19:19:25 -0700

    Implement Task Fragment

--- app/src/main/java/joker/persona/ngrocken/androidudemyshowcaseapp/MainActivity.java
+++ app/src/main/java/joker/persona/ngrocken/androidudemyshowcaseapp/MainActivity.java
@@ -1,15 +1,36 @@
 package joker.persona.ngrocken.androidudemyshowcaseapp;
 
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FrgTasks.OnFragmentInteractionListener {
+
+    private FrgTasks taskFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        taskFragment = new FrgTasks();
+        setUpFragment(taskFragment);
+
+    }
+
+    private void setUpFragment(Fragment fragment) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        if(fragment.isAdded()) {
+            return;
+        }
+        ft.replace(R.id.main_fragment_cont, fragment);
+        ft.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
     }
 }
